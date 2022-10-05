fdc2e43f017e19ec70b9fbcd92bb46e21906e752
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-05 04:28:17 -0500

    final changes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-const sequelize = require('./db/config')
-const api = require('./routes/api.routes')
+const sequelize = require('./src/db/config')
+const api = require('./src/routes/api.routes')
+const cors = require('cors')
 
 const port = 1234
 
+const corsOptions = {
+    origin: '*',
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use( express.json() )
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', api)
